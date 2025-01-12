ae049cf182d3e5311358460d520f61a84cba6df4
ramah-madi <ramahmadi2001@gmail.com>
2025-01-12 18:39:40 +0300

    Add nav and footer components for all the pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import chefB from '../assets/restaurantChefB.jpg'
+
 export default function Footer() {
   return (
     <footer>
-      <img className='footer-image' src={chefB} alt='A chef making food in the kitchen'/>
+      <img className='footer-image' src={chefB} style={{ height: '20rem' }} alt='A chef making food in the kitchen'/>
       <ul className='doormat-navigation'>
-        <li>Doormat Navigation</li>
+        <li className='footer-title'>Doormat Navigation</li>
         <li><a href='/'>Home</a></li>
         <li><a href='/about'>About</a></li>
         <li><a href='/menu'>Menu</a></li>
@@ -15,14 +16,14 @@ export default function Footer() {
       </ul>
 
       <ul className='contact'>
-        <li>Contact</li>
+        <li className='footer-title'>Contact</li>
         <li><a href='/address'>Address</a></li>
         <li><a href='/phone-number'>Phone Number</a></li>
         <li><a href='/email'>Email</a></li>
       </ul>
 
       <ul className='social-media-links'>
-        <li>Social Media Links</li>
+        <li className='footer-title'>Social Media Links</li>
         <li><a href='/facebook'>Facebook</a></li>
         <li><a href='/instagram'>Instagram</a></li>
         <li><a href='/linked-in'>Linked In</a></li>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,14 @@ import logo from '../assets/Logo.svg';
 export default function Nav() {
   return (
     <nav>
+      <img className='logo' src={logo} alt='Logo of the little lemon retaurant'/>
       <ul>
-        <img className='Logo' src={logo} alt='Logo of the little lemon retaurant'/>
         <li><a href='/'>Home</a></li>
         <li><a href='/about'>About</a></li>
-        <li><a href='/contact'>Contact</a></li>
+        <li><a href='/menu'>Menu</a></li>
+        <li><a href='/reservations'>Reservations</a></li>
+        <li><a href='/order-online'>Order Online</a></li>
+        <li><a href='/login'>Login</a></li>
       </ul>
     </nav>
   )
