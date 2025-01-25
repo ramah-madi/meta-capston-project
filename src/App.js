a8baff26f838cd3ccd452ff4c4317d53df3dc2dd
ramah-madi <ramahmadi2001@gmail.com>
2025-01-25 20:42:14 +0300

    Add routes components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Home from './pages/Home';
+import ReservationMain from './pages/ReservationMain';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Nav />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reservations" element={<ReservationMain />} />
+      </Routes>
       <Footer />
     </>
   );
